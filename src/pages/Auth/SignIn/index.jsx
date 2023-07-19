5e2a5caeb83227fc7e98728e296dace0c078b538
AmanturUl <amanturulukbekov06@gmail.com>
2023-07-19 18:58:49 +0600

    Added: login and account registration pages

--- src/pages/Auth/SignIn/index.jsx
+++ src/pages/Auth/SignIn/index.jsx
@@ -1,11 +1,91 @@
 import React from 'react'
 
-const SignIn = () => {
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
+
+const SignInPage = () => {
+  const [showPassword, setShowPassword] = useState(false)
+
   return (
-    <>
-      <h1>SignInPage</h1>
-    </>
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}
+      boxShadow="dark-lg" rounded="md">
+
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'} textAlign={'center'}>
+            Sign in
+          </Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            to resume enjoying all of our cool features ✌️
+          </Text>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input type={showPassword ? 'text' : 'password'} />
+                <InputRightElement h={'full'}>
+                  <Button
+                    variant={'ghost'}
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }>
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                loadingText="Submitting"
+                size="lg"
+                bg={'#FE3796'}
+                color={'white'}
+                _hover={{
+                  bg: '#D92770',
+                }}>
+                Sign in
+              </Button>
+            </Stack>
+            <Stack pt={6}>
+              <Flex align={'center'}>
+                Don&apos;t have an account? <Link to="/auth/signup"><Text ml={'2'} color={'blue.400'}>Sign up</Text></Link>
+              </Flex>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
   )
 }
 
-export default SignIn
+export default SignInPage
